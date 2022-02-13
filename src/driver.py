340d7094af1b4c493843b8f86417608bae42492f
BlakeGerard <blakegerard@ou.edu>
2022-02-13 00:02:59 -0600

    Showing the state demo

--- src/driver.py
+++ src/driver.py
@@ -3,5 +3,10 @@ import numpy as np
 import cv2 as cv
 
 srv = StateServer()
-#srv.start()
+srv.start()
 state = srv.get_state()
+
+cv.imshow("State", state)
+cv.waitKey(0)
+cv.destroyAllWindows()
+cv.waitKey(1)
