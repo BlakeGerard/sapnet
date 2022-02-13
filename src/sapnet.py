c061024f899cc56e01a711851da814ada112d48d
BlakeGerard <blakegerard@ou.edu>
2022-02-13 16:30:05 -0600

    Going to start with simple DQN network

--- src/sapnet.py
+++ src/sapnet.py
@@ -1,5 +1,52 @@
+import math
+import random
 import numpy as np
+from collections import namedtuple, deque
 
-class SAPNet:
-    def __init__():
-        pass
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+    
+    def __len__(self):
+        return len(self.memory)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+class SAPNetDQN(nn.Module):
+
+    def __init__(self, w, h, outputs):
+        super(SAPNetDQN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+
+        # Compute linear input connections
+        def conv2d_size_out(size, kernel_size=5, stride=2):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+        linear_input_size = convw * convh * 32
+
+        self.head = nn.Linear(linear_input_size, outputs)
+    
+    def forward(self, x):
+        x = x.to(device)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        return self.head(x.view(x.size(0), -1))
